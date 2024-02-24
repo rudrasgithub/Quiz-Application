f7427b44d4051b0de313b24f9bb10d67f4610029
rudrasgithub <rudramanaidu99@gmail.com>
2024-02-24 10:16:44 +0530

    first commit

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export default async function connect(){
-    await mongoose.connect("mongodb+srv://rudramanaidupasupuleti:rudra9999@cluster0.rcgmcse.mongodb.net/")
+    await mongoose.connect("mongodb+srv://rudramanaidupasupuleti:rudra9999@cluster0.rcgmcse.mongodb.net/quiz-application")
     console.log("Database Connected")
 }
